3738a7221bfcdc009698acd2be8885e514febffd
Sara Johnsson <sara86persson@hotmail.com>
2024-08-23 14:45:04 +0200

    fixed tab size

--- typescript/classes/Player.ts
+++ typescript/classes/Player.ts
@@ -1,10 +1,10 @@
 export default class Player {
 
-    name: string;
-    symbol: string;
+	name: string;
+	symbol: string;
 
-    constructor(name: string, symbol: string) {
-        this.name = name;
-        this.symbol = symbol;
-    }
+	constructor(name: string, symbol: string) {
+		this.name = name;
+		this.symbol = symbol;
+	}
 }
